853e6e4366ee1b3753dfdeb243120557d5ecb344
John Duimovich <jduimovich@gmail.com>
2022-06-05 13:10:05 -0400

    pretty

--- run.sh
+++ run.sh
@@ -11,26 +11,50 @@ done
 echo "$COUNTER demos found."  
 readarray -t sorted < <(for a in "${!DEMOS[@]}"; do echo "$a"; done | sort)
  
+SHOWSTATUS=
 CHOICE=
 until [ "${CHOICE^}" != "" ]; do
     echo -n 
     clear
     cat banner
-    echo "--------------------------------"
+   
+    if [ "$SHOWSTATUS" = "yes" ]; then
+        SHOWSTATUS=no
+        echo "--------------------------------"
+        echo "STATUS "
+        kubectl get Application ${DEMOS[$key]} -n ${DEMOS[$key]} &> /dev/null
+        ERR=$? 
+        if [  "$ERR" == "0" ]
+        then
+            RT=$(oc get routes ${DEMOS[$key]} -n ${DEMOS[$key]}  -o yaml  2>/dev/null | yq '.spec.host')
+            printf " (Installed)  Running at https://$RT\n" 
+        else 
+            printf "${DEMOS[$key]} not running\n"
+        fi 
+    fi 
+     echo "--------------------------------"
     for key in ${!sorted[@]} 
-    do 
-        echo "$key : ${DEMOS[$key]}" 
+    do  
+        printf "%3s: %-20s \n"  $key  ${DEMOS[$key]}
     done 
-    read -n1 -p "Choose Demo (q to quit): "  SELECT 
+    read -n1 -p "Choose Demo (q to quit, s for status): "  SELECT 
     if [ "$SELECT" = "q" ]; then 
+        echo 
         echo "Exiting..."
         exit
-    fi  
-    SELECT=${SELECT^}
-    if [ -n "$SELECT" ]; then
-        CHOICE=${DEMOS[$SELECT]}
+    fi 
+    if [ "$SELECT" = "s" ]; then
+        SHOWSTATUS=yes 
+    fi
+    SELECT=${SELECT^} 
+    CHOICE=${DEMOS[$SELECT]}
+    if [ -n "$CHOICE" ]; then
+        echo; echo "Demo chosen is $CHOICE"
+        ./hack/e2e.sh demos/$CHOICE
+        CHOICE=""
+    else      
+        clear
+        cat banner
     fi 
     echo;
 done  
-echo "Demo chosen is $CHOICE"
-./hack/e2e.sh demos/$CHOICE
